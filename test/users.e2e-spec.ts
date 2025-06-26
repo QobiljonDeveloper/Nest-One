be8b958e0d379bfd37a83f1f51ceb58b38d1f374
QobiljonYuldashev <qobiljonyuldashev044>
2025-06-26 16:47:52 +0500

    change

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -7,6 +7,7 @@ describe("User (e2e)", () => {
   let app: INestApplication;
   let token: String;
 
+  jest.setTimeout(15000);
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -34,5 +35,46 @@ describe("User (e2e)", () => {
       .set("Authorization", `Bearer ${token}`)
       .expect("Content-Type", /json/)
       .expect(200);
+  });
+
+  it("/users (GET) --> 401 OK", () => {
+    return (
+      request(app.getHttpServer())
+        .get("/users")
+        //  .set("Authorization", `Bearer ${token}`)
+        .expect("Content-Type", /json/)
+        .expect(401)
+    );
+  });
+
+  it("/auth/signup (POST) --> 201", () => {
+    return request(app.getHttpServer())
+      .post("/auth/signup")
+      .send({
+        name: "user123",
+        email: "user123123@gmail.com",
+        password: "123456789876543",
+        value: "admin",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201);
+  });
+
+  it("/auth/signup (POST) --> 400", () => {
+    return request(app.getHttpServer())
+      .post("/auth/signup")
+      .send({
+        name: "user123",
+        email: "user1231232@gmail.com",
+        password: "123456789876543",
+        value: "admin",
+      })
+      .expect("Content-Type", /json/)
+      .expect(409)
+      .expect({
+        message: "Bunday foydalanuvchi bor",
+        error: "Conflict",
+        statusCode: 409,
+      });
   });
 });
